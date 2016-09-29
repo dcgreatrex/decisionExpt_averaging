8e98416d6e55aedeeb2946f3c02682299cbee679
dcgretarex <dcgreatrex@aol.com>
2016-09-29 15:52:19 +0100

    edit main

--- averaging.m
+++ averaging.m
@@ -14,9 +14,7 @@ try
     PARENT_practiceLoop(Pointers, Stimuli, Response, Prepair);
     %---------------------
     % call staircasing procedure 
-    threshold = PARENT_staircaseMain(subNo, Pointers, Response, Prepair, Stimuli); % 1.5; 1.045; 0.85; 1.32
-    %threshold = 1.7;
-    %noisecentering(Stimuli, Pointers);
+    threshold = PARENT_staircaseMain(subNo, Pointers, Response, Prepair, Stimuli);
     %---------------------
     % call main experimental procedure  
     PARENT_trialLoop(Pointers, Stimuli, Response, Prepair, threshold);       
